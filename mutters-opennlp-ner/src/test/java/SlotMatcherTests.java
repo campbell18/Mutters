149faa076894be205375e7b76b7717ea36d1e9cf
AIRNZ-NZ\foskic <Campbell.Foskin@airnz.co.nz>
2018-07-06 13:32:02 +1200

    Added test data to classpath.

--- mutters-opennlp-ner/src/test/java/SlotMatcherTests.java
+++ mutters-opennlp-ner/src/test/java/SlotMatcherTests.java
@@ -6,7 +6,6 @@ import com.rabidgremlin.mutters.opennlp.ner.OpenNLPSlotMatcher;
 import com.rabidgremlin.mutters.opennlp.intent.OpenNLPTokenizer;
 import org.junit.Test;
 import opennlp.tools.tokenize.SimpleTokenizer;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import java.util.HashMap;
@@ -17,7 +16,7 @@ public class SlotMatcherTests
   public void when_default_slot_does_not_match_return_default_value()
   {
     OpenNLPSlotMatcher slotMatcher = new OpenNLPSlotMatcher(new OpenNLPTokenizer(SimpleTokenizer.INSTANCE));
-    slotMatcher.addSlotModel("testSlot", "nerModel");
+    slotMatcher.addSlotModel("testSlot", "models/en-ner-persons.bin");
     Intent testIntent = new Intent("testIntent");
     TestSlot testSlot = new TestSlot("testSlot");
     testIntent.addSlot(testSlot);
